4b7afde79269b774fddc320bb7646772ee1eeb9e
Sukrita Janseetong <iam_sukrita@hotmail.com>
2020-05-20 17:43:23 +0700

    As6 3-1

--- Assignment 3/3-1/3-1.ino
+++ Assignment 3/3-1/3-1.ino
@@ -3,20 +3,11 @@ String state = "ON";
 void setup()
 {
     Serial.begin(9600);
-    pinMode (2 , OUTPUT);
-    pinMode (3 , OUTPUT);
-    pinMode (4 , OUTPUT);
-    pinMode (5 , OUTPUT);
-    pinMode (6 , OUTPUT);
-    pinMode (7 , OUTPUT);
-    pinMode (8 , OUTPUT);
-    pinMode (9 , OUTPUT);
-    pinMode (10 , OUTPUT);
-    pinMode (11 , OUTPUT);
-    pinMode (12 , OUTPUT);
-    pinMode (13 , OUTPUT);
+   int x;
+    for (x=2; x<13; x++) {
+    pinMode(x, OUTPUT);
+    }   
 }
-
 void loop()
 {
     long currentTime = millis();
