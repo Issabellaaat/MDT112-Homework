084059180229e7fad5f04019a69a0f20a1c97290
Sukrita Janseetong <iam_sukrita@hotmail.com>
2020-05-20 17:53:19 +0700

    3-7

--- Assignment 3/3-7/3-7.ino
+++ Assignment 3/3-7/3-7.ino
@@ -2,19 +2,10 @@ void setup()
 {
   Serial.begin(9600);
 
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
-
+  int x;
+    for (x=2; x<13; x++) {
+    pinMode(x, OUTPUT);
+    }   
 }
 
 int x = 1;
@@ -47,18 +38,10 @@ void loop()
   }
   x++;
   if (x > 100) {
-    digitalWrite(2, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0);
-    digitalWrite(8, 0);
-    digitalWrite(9, 0);
-    digitalWrite(10, 0);
-    digitalWrite(11, 0);
-    digitalWrite(12, 0);
-    digitalWrite(13, 0);
+      int y;
+    for (y=2; y<13; y++) {
+    pinMode(y,0);
+    }   
     delay(1000);
     x = 1;
   }
