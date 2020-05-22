d998f33e9d0af19afc93904f22811fc15ab6db3e
Sukrita Janseetong <iam_sukrita@hotmail.com>
2020-05-22 16:56:40 +0700

    3-5

--- Assignment 3/3-3/3-3.ino
+++ Assignment 3/3-3/3-3.ino
@@ -6,43 +6,15 @@ void setup(){
 }
 
 void loop(){
-    
-    digitalWrite ( 2 , 1);
-    delay (250);
-    digitalWrite ( 3 , 1);
-    delay (250);
-    digitalWrite ( 4 , 1);
-    delay (250);
-    digitalWrite ( 5 , 1);
-    delay (250);
-    digitalWrite ( 6 , 1);
-    delay (250);
-    digitalWrite ( 7 , 1);
-    delay (250);
-    digitalWrite ( 8 , 1);
-    delay (250);
-    digitalWrite ( 9 , 1);
-    delay (250);
-    digitalWrite ( 10 , 1);
-    delay (250);
-    digitalWrite ( 11 , 1);
-    delay (250);
-    digitalWrite ( 12 , 1);
-    delay (250);
-    digitalWrite ( 13 , 1);
-    delay (250);
+   for (x=2; x<=13; x++) {
+        delay (250);
+    digitalWrite(x, 1);    
+        delay (250);
+        }
+      
+    for (x=2; x<=13; x++) { 
+    digitalWrite(x, 0); 
+        }   
+        delay (250);
 
-    digitalWrite ( 2 , 0);
-    digitalWrite ( 3 , 0);
-    digitalWrite ( 4 , 0);
-    digitalWrite ( 5 , 0);
-    digitalWrite ( 6 , 0);
-    digitalWrite ( 7 , 0);
-    digitalWrite ( 8 , 0);
-    digitalWrite ( 9 , 0);
-    digitalWrite ( 10 , 0);
-    digitalWrite ( 11 , 0);
-    digitalWrite ( 12 , 0);
-    digitalWrite ( 13 , 0);
-    delay (250);
 }
